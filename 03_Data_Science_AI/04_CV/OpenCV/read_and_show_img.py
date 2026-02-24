1b38a6b1dc50b3e5000724492d2ff963d68df0d9
Cinna <cinsophie2023@gmail.com>
2026-02-24 19:57:18 +0800

    added code for read and show img

--- 03_Data_Science_AI/04_CV/OpenCV/read_and_show_img.py
+++ 03_Data_Science_AI/04_CV/OpenCV/read_and_show_img.py
@@ -5,6 +5,10 @@ img = cv2.imread("sample_img_guide.png", cv2.IMREAD_COLOR)
 
 cv2.imshow("Gudetama", img); #displays img in a window
 
+filename ="coolkog_gudetama.png"
+
+cv2.imwrite(filename, img) #save image @local
+
 cv2.waitKey(0) #waits till a key is pressed; needed or window will close immediately
 
 cv2.destroyAllWindows(); #closes after key press
